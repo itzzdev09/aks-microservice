4c3386463315458feddce1d2f883b4ee903a09c1
itzzdev09 <devved90@gmail.com>
2025-08-13 00:17:02 +0530

    Initial commit - Docker microservice with K6 test

---
+++ src/server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from AKS!'));
+app.listen(80, () => console.log('Server running on port 80'));
